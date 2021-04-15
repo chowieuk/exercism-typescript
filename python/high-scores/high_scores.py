47a8d39179a7549609e5c141f93d85d0618fb2c1
Christopher Howie <christopher.howie@gmail.com>
2021-04-14 22:42:31 -0700

    Updated high scores with alternate solution. More elegant, but higher big O complexity

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -6,8 +6,9 @@ def personal_best(scores):
     return max(scores)
 
 def personal_top_three(scores):
-    top_three = []
-    while len(scores) > 0 and len(top_three) < 3:
-        top_three.append(max(scores))
-        scores.remove(max(scores))
-    return top_three
+    # top_three = []
+    # while len(scores) > 0 and len(top_three) < 3:
+    #     top_three.append(max(scores))
+    #     scores.remove(max(scores))
+    # return top_three
+    return sorted(scores, reverse=True)[:3] # sorts the list into reverse order, then returns the first 3 items
