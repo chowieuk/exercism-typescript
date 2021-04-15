3501b20844c8d02237b158426af955d0aff0e43f
Christopher Howie <christopher.howie@gmail.com>
2021-04-14 22:34:51 -0700

    Completed high scores

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,13 @@
+def latest(scores):
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+def personal_top_three(scores):
+    top_three = []
+    while len(scores) > 0 and len(top_three) < 3:
+        top_three.append(max(scores))
+        scores.remove(max(scores))
+    return top_three
