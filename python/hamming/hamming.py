d3d1875d78667afe7b50725a7b719653f23f47a4
Christopher Howie <christopher.howie@gmail.com>
2021-04-23 00:31:45 -0700

    added zip solution to hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -5,11 +5,16 @@ def distance(strand_a, strand_b):
     if strand_a == strand_b:
         return 0
     
-    distance = 0
+    hamming_distance = 0
     # using iteration
-    i = 0 
-    while i < len(strand_a):
-        if strand_a[i] != strand_b[i]:
-            distance += 1
-        i += 1
-    return distance
+    # i = 0 
+    # while i < len(strand_a):
+    #     if strand_a[i] != strand_b[i]:
+    #         hamming_distance += 1
+    #     i += 1
+    
+    # using zip
+    for x, y in zip(strand_a, strand_b):
+        if x != y:
+            hamming_distance += 1
+    return hamming_distance
