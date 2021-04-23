3f12726a3058c8529b081a5b68e55e7331543edc
Christopher Howie <christopher.howie@gmail.com>
2021-04-23 01:13:33 -0700

    Not sure why this wasn't committed. I need to learn how to revert and such

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,7 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:   
+    clean_string = string.replace("-","").replace(" ","").casefold()
+    print(clean_string)
+    for char in clean_string:
+        if clean_string.count(char) > 1:
+            return False            
+    return True
