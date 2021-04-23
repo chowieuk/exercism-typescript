50b2b2164d0a5c21ef6c54afd3189938e06edf5f
Christopher Howie <christopher.howie@gmail.com>
2021-04-23 00:37:56 -0700

    added for loop iteration solution to hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -6,15 +6,24 @@ def distance(strand_a, strand_b):
         return 0
     
     hamming_distance = 0
+    i = 0 
+
+    # using iteration with a for loop
+
+    for i in range(0, len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                hamming_distance += 1
+            i += 1
+    
     # using iteration
-    # i = 0 
     # while i < len(strand_a):
     #     if strand_a[i] != strand_b[i]:
     #         hamming_distance += 1
     #     i += 1
     
     # using zip
-    for x, y in zip(strand_a, strand_b):
-        if x != y:
-            hamming_distance += 1
+    # for x, y in zip(strand_a, strand_b):
+    #     if x != y:
+    #         hamming_distance += 1
+
     return hamming_distance
