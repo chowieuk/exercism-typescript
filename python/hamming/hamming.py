b8306e4a47cf1db11902a5dd8635c99a48a8aa5d
Christopher Howie <christopher.howie@gmail.com>
2021-04-23 00:16:22 -0700

    Cleaning up

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,15 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError ("Strands must be the same length")
+
+    if strand_a == strand_b:
+        return 0
+    
+    distance = 0
+    # using iteration
+    i = 0 
+    while i < len(strand_a):
+        if strand_a[i] != strand_b[i]:
+            distance += 1
+        i += 1
+    return distance
