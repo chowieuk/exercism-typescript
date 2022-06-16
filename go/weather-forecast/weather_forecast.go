7d40c6a22e942383acc8459a8fdb9c92582d2310
Christopher Howie <christopher.howie@gmail.com>
2022-06-15 17:20:03 -0700

    finished weather forecasting

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,14 @@
+// Package weather provides functions to 
+// forecast the current weather condition of various cities in Goblinocus.
 package weather
 
+// CurrentCondition represents the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation represents the current location.
 var CurrentLocation string
 
+// Forecast returns the weather forecast for a given city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
