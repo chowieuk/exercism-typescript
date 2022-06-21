dc4c5e92f36608d377fea6f4d162a8a85c90d89d
Christopher Howie <christopher.howie@gmail.com>
2022-06-20 19:41:16 -0700

    another itteration on blackjack using a map

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -2,35 +2,23 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-	case "ace":
-		return 11
-	case "eight":
-		return 8
-	case "two":
-		return 2
-	case "nine":
-		return 9
-	case "three":
-		return 3
-	case "ten":
-		return 10
-	case "four":
-		return 4
-	case "jack":
-		return 10
-	case "five":
-		return 5
-	case "queen":
-		return 10
-	case "six":
-		return 6
-	case "king":
-		return 10
-	case "seven":
-		return 7
+	cardValue := map[string]int{
+		"ace":   11,
+		"eight": 8,
+		"two":   2,
+		"nine":  9,
+		"three": 3,
+		"ten":   10,
+		"four":  4,
+		"jack":  10,
+		"five":  5,
+		"queen": 10,
+		"six":   6,
+		"king":  10,
+		"seven": 7,
+		"one":   1,
 	}
-	return 0
+	return cardValue[card]
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
