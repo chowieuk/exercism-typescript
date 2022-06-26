17149cd40cdce7d93960e9ff53b6dbb4292425f5
Christopher Howie <christopher.howie@gmail.com>
2022-06-26 10:26:12 -0700

    finished interset is interesting

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -1,21 +1,39 @@
 package interest
 
+/*
+1.621% for a balance greater than or equal to 1000 dollars, and less than 5000 dollars.
+2.475% for a balance greater than or equal to 5000 dollars.
+*/
+
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance >= 0 && balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	}
+	return 2.475
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * float64(InterestRate(balance)) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	i := 0
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		i++
+	}
+	return i
 }
