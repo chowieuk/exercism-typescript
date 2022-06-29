bbc2882ab2321ce50986aa3652072e618c70beab
Christopher Howie <christopher.howie@gmail.com>
2022-06-29 14:00:23 -0700

    Completed the farm

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,10 +1,46 @@
 package thefarm
 
+import (
+	"errors"
+	"fmt"
+)
+
 // See types.go for the types defined for this exercise.
 
 // TODO: Define the SillyNephewError type here.
+type SillyNephewError struct {
+	cows int
+}
+
+func (e *SillyNephewError) Error() string {
+	return fmt.Sprintf("silly nephew, there cannot be %d cows", e.cows)
+}
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+
+	if cows < 0 {
+		return 0, &SillyNephewError{
+			cows: cows,
+		}
+	}
+
+	fodder, err := weightFodder.FodderAmount()
+
+	if fodder < 0 && (err == nil || err == ErrScaleMalfunction) {
+		return 0, errors.New("negative fodder")
+	}
+
+	if err != nil {
+		if err == ErrScaleMalfunction && fodder > 0 {
+			return 2 * fodder / float64(cows), nil
+		}
+		return 0, err
+	}
+
+	if cows == 0 {
+		return 0, errors.New("division by zero")
+	}
+
+	return fodder / float64(cows), nil
 }
