884dcac07aedcdd9fca020b83a7533e27f0617b9
Christopher Howie <christopher.howie@gmail.com>
2022-06-30 07:27:43 -0700

    Improved regex based on Bytamines solution

--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -6,31 +6,28 @@ import (
 )
 
 func IsValidLine(text string) bool {
-	re := regexp.MustCompile(`^\[TRC\]|^\[DBG\]|^\[INF\]|^\[WRN\]|^\[ERR\]|^\[FTL\]`)
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
 	return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	re := regexp.MustCompile(`<([=\-~\*]*)>`)
+	re := regexp.MustCompile(`<[-=~*]*>`)
 	return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	re1 := regexp.MustCompile(`"[^"]+"`)
-	re2 := regexp.MustCompile(`(?i)password`)
+	re := regexp.MustCompile(`(?i)".*password.*"`)
 	count := 0
 	for _, line := range lines {
-		if re1.MatchString(line) {
-			if re2.MatchString(re1.FindString(line)) {
-				count++
-			}
+		if re.MatchString(line) {
+			count++
 		}
 	}
 	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	re := regexp.MustCompile(`(end-of-line)(\d+)`)
+	re := regexp.MustCompile(`end-of-line\d+`)
 	return re.ReplaceAllString(text, "")
 }
 
@@ -39,13 +36,11 @@ func TagWithUserName(lines []string) []string {
 	re := regexp.MustCompile(`User[[:blank:]]+(\w+)`)
 	var newLines []string
 	for _, line := range lines {
-		if re.MatchString(line) {
-			newLine := fmt.Sprintf("[USR] %s ", re.FindStringSubmatch(line)[1]) + line
-			newLines = append(newLines, newLine)
-			continue
+		founds := re.FindStringSubmatch(line)
+		if founds != nil {
+			line = fmt.Sprintf("[USR] %s %s", founds[1], line)
 		}
 		newLines = append(newLines, line)
 	}
-
 	return newLines
 }
