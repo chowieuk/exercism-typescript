5185ffce1d6d0c495d4c99a5e408e0c813e229b0
Christopher Howie <christopher.howie@gmail.com>
2022-07-18 22:10:31 -0700

    Passed tests

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,20 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var result string
+	if number%3 == 0 {
+		result += "Pling"
+	}
+	if number%5 == 0 {
+		result += "Plang"
+	}
+	if number%7 == 0 {
+		result += "Plong"
+	}
+	if result == "" {
+		return fmt.Sprint(number)
+	}
+	return result
 }
