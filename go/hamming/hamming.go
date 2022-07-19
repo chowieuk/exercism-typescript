b54fb83f9727423d9b758f3df065d89d0dfb613b
Christopher Howie <christopher.howie@gmail.com>
2022-07-18 17:32:29 -0700

    Passed tests

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("Length of given strings do not match")
+	}
+	aChars := []rune(a)
+	bChars := []rune(b)
+	var total int
+	for i, char := range aChars {
+		if char != bChars[i] {
+			total++
+		}
+	}
+	return total, nil
 }
