c051ee69e297b363b6361804e08c2e895121fdb5
Christopher Howie <christopher.howie@gmail.com>
2022-07-18 22:56:15 -0700

    Adjusted solution using a map. Initial attempt didn't work due to lowercase test cases

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,37 +1,40 @@
 package scrabble
 
-import (
-	"errors"
-	"unicode"
-)
+import "unicode"
 
-func ParseChar(c rune) (int, error) {
-	switch unicode.ToUpper(c) {
-	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-		return 1, nil
-	case 'D', 'G':
-		return 2, nil
-	case 'B', 'C', 'M', 'P':
-		return 3, nil
-	case 'F', 'H', 'V', 'W', 'Y':
-		return 4, nil
-	case 'K':
-		return 5, nil
-	case 'J', 'X':
-		return 8, nil
-	case 'Q', 'Z':
-		return 10, nil
-	}
-	return 0, errors.New("Unrecognized Character")
+var letters = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
 }
 
 func Score(word string) int {
 	var result int
-	var value int
-	for _, char := range []rune(word) {
-		value, _ = ParseChar(char)
-		result += value
+	for _, char := range word {
+		result += letters[unicode.ToUpper(char)]
 	}
-
 	return result
 }
