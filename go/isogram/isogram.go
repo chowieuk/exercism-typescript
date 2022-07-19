12af9f17a2a1ea16fb317b9fbb7d6dc086ad5664
Christopher Howie <christopher.howie@gmail.com>
2022-07-19 13:17:02 -0700

    First iteration passing tests. Used two for loops

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,25 @@
 package isogram
 
+import "unicode"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+
+	if word == "" {
+		return true
+	}
+
+	var CharCount []rune
+
+	for _, c := range word {
+		if c == '-' || c == ' ' {
+			continue
+		}
+		for _, v := range CharCount {
+			if unicode.ToLower(v) == unicode.ToLower(c) {
+				return false
+			}
+		}
+		CharCount = append(CharCount, c)
+	}
+	return true
 }
