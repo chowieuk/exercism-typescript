b48aeda52b76c6c5eabe4e7e773eac6eb9d3c82a
Christopher Howie <christopher.howie@gmail.com>
2022-07-19 21:24:54 -0700

    First iteration. Passed tests.

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,21 @@
 package diffsquares
 
+// Returns the square of the sum of first n natural numbers
+// Sum of n natural numbers is
+// S=(1/2)(n)(n+1).
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return sum
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
