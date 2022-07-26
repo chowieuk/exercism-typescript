3926dba2ce9e5f2c1430a2dead90bfcadd4e875e
Christopher Howie <christopher.howie@gmail.com>
2022-07-25 21:01:35 -0700

    Passed tests

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,38 @@
 package luhn
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+
+	var sum int
+	j := 1
+
+	id = strings.ReplaceAll(id, " ", "")
+
+	if len(id) <= 1 {
+		return false
+	}
+
+	for i := len(id) - 1; i >= 0; i-- {
+		if !unicode.IsNumber(rune(id[i])) {
+			return false
+		}
+		digit := int(id[i] - '0')
+		if j%2 == 0 {
+			if (digit * 2) > 9 {
+				sum += (digit * 2) - 9
+			} else {
+				sum += (digit * 2)
+			}
+			j++
+		} else {
+			sum += digit
+			j++
+		}
+	}
+
+	return sum%10 == 0
 }
