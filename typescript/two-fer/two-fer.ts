f54b6ae3d2187a06e95d8d1d5695c9b210b07b4a
Christopher Howie <christopher.howie@gmail.com>
2022-07-27 10:36:31 -0700

    Set a default value for name instead of checking if name is undefined

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -2,6 +2,6 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(name?: string): string {
-  return `One for ${name || "you"}, one for me.`
+export function twoFer(name: string = "you"): string {
+  return `One for ${name}, one for me.`
 }
