75c4c295b6884f48efee63abbedacab8963a9649
Christopher Howie <christopher.howie@gmail.com>
2022-07-27 12:55:58 -0700

    Passed tests

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,20 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+const resistorValue = new Map<string, string>([
+  ["black","0"],
+  ["brown","1"],
+  ["red","2"],
+  ["orange","3"],
+  ["yellow","4"],
+  ["green","5"],
+  ["blue","6"],
+  ["violet","7"],
+  ["grey","8"],
+  ["white","9"]
+])
+
+export function decodedValue(colors: string[]): number {
+  let result: string = "";
+  if ((resistorValue.get(colors[0]) != undefined) && (resistorValue.get(colors[1]) != undefined)) {
+    result  = `${resistorValue.get(colors[0])}${resistorValue.get(colors[1])}`;
+  }
+  return parseInt(result)
 }
