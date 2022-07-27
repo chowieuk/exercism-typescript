7b9b739f9761dc3054c4ff3c4c017d80f8edfb49
Christopher Howie <christopher.howie@gmail.com>
2022-07-27 13:28:07 -0700

    Second iteration using enum and Color type declartion

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,20 +1,18 @@
-const resistorValue = new Map<string, string>([
-  ["black","0"],
-  ["brown","1"],
-  ["red","2"],
-  ["orange","3"],
-  ["yellow","4"],
-  ["green","5"],
-  ["blue","6"],
-  ["violet","7"],
-  ["grey","8"],
-  ["white","9"]
-])
+export enum ResistorValues {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9
+}
 
-export function decodedValue(colors: string[]): number {
-  let result: string = "";
-  if ((resistorValue.get(colors[0]) != undefined) && (resistorValue.get(colors[1]) != undefined)) {
-    result  = `${resistorValue.get(colors[0])}${resistorValue.get(colors[1])}`;
-  }
-  return parseInt(result)
+type Color = keyof typeof ResistorValues;
+
+export function decodedValue([first,second] : Color[]): number {
+  return Number(`${ResistorValues[first]}${ResistorValues[second]}`)
 }
