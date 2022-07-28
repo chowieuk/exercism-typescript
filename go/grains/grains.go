f0fb77de64c1289c8f257a34db7025c4f7ae1301
Christopher Howie <christopher.howie@gmail.com>
2022-07-28 16:28:44 -0700

    Passed tests Power and iteration approach

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,22 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, errors.New("Invalid number")
+	}
+	return uint64((math.Pow(2, float64(number-1)))), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	var result uint64 = 0
+	for i := 1; i <= 64; i++ {
+		squareValue, _ := Square(i)
+		result += squareValue
+	}
+	return result
 }
