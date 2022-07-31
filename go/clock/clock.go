5a3f1c5f6402ccba07142a55e926b428b57f5e83
Christopher Howie <christopher.howie@gmail.com>
2022-07-31 14:22:46 -0700

    Passed tests

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,81 @@
 package clock
 
+import "fmt"
+
+// norm returns nhi, nlo such that
+//	hi * base + lo == nhi * base + nlo
+//	0 <= nlo < base
+// taken from time.go
+func norm(hi, lo, baselo int) (nhi, nlo int) {
+	if lo < 0 {
+		n := (-lo-1)/baselo + 1
+		hi -= n
+		lo += n * baselo
+	}
+	if lo >= baselo {
+		n := lo / baselo
+		hi += n
+		lo -= n * baselo
+	}
+
+	return hi, lo
+}
+
+// overflow nlo such that
+//  0 <= nlo < base
+// ensures that lo rolls over in increments of base
+// adapted from above
+
+func overflow(lo, base int) (nlo int) {
+	if lo < 0 {
+		n := (-lo-1)/base + 1
+		lo += n * base
+	}
+	if lo >= base {
+		n := lo / base
+		lo -= n * base
+	}
+	return lo
+}
+
 // Define the Clock type here.
 
+type Clock struct {
+	hours   int
+	minutes int
+}
+
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	var c Clock
+
+	h, m = norm(h, m, 60)
+	h = overflow(h, 24)
+
+	c.hours = h
+	c.minutes = m
+	return c
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	if m+c.minutes >= 60 {
+		c.hours, c.minutes = norm(c.hours, m+c.minutes, 60)
+		c.hours = overflow(c.hours, 24)
+	} else {
+		c.minutes += m
+	}
+	return c
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	if c.minutes-m < 0 {
+		c.hours, c.minutes = norm(c.hours, c.minutes-m, 60)
+		c.hours = overflow(c.hours, 24)
+	} else {
+		c.minutes -= m
+	}
+	return c
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
 }
