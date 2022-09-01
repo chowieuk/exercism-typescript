b3fc6e76cde970c7ffc1fc6cf2bacc6d492cb35f
Christopher Howie <christopher.howie@gmail.com>
2022-09-01 15:29:57 -0700

    Passed tests

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,17 @@
 package etl
 
+import (
+	"strings"
+)
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	var result = make(map[string]int)
+
+	for score, letters := range in {
+		for _, letter := range letters {
+			result[strings.ToLower(letter)] = score
+		}
+	}
+
+	return result
 }
