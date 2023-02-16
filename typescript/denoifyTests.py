116ce945f12b8c599c446cb42e0fae00c107df53
chowie <christopher.howie@gmail.com>
2023-02-16 12:23:47 +0800

    added replacement for toBe()

--- typescript/denoifyTests.py
+++ typescript/denoifyTests.py
@@ -11,8 +11,12 @@ data.insert(1,"""import { describe, it } from "https://deno.land/std@0.177.0/tes
 import { assertEquals } from "https://deno.land/std@0.135.0/testing/asserts.ts";""")
 for i, line in enumerate(data):
     if line.startswith('    expect('):
-        line = line.replace('expect(','assertEquals(')
-        line = line.replace(').toEqual(',',')
+        if line.count('toEqual(') > 0:
+            line = line.replace('expect(','assertEquals(')
+            line = line.replace(').toEqual(',',')
+        elif line.count('toBe(') > 0:
+            line = line.replace('expect(','assertEquals(')
+            line = line.replace(').toBe(',',')
         data[i] = line
 
 with open(sys.argv[1], 'w') as file:
