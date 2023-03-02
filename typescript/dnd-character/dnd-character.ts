49df79e8f31c66e2778a9054bfa38bfa46d81c83
chowie <christopher.howie@gmail.com>
2023-03-02 17:24:50 +0800

    passing tests

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,28 @@
+function Roll() {
+  return Math.ceil(Math.random() * 6);
+}
+
 export class DnDCharacter {
+
   public static generateAbilityScore(): number {
-    throw new Error("Remove this statement and implement this function");
+    let first = Roll();
+    let second = Roll();
+    let third = Roll();
+    let fourth = Roll();
+
+    return first + second + third + fourth - Math.min(...[first, second, third, fourth]);
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error("Remove this statement and implement this function");
+    return Math.floor((abilityValue - 10) / 2)
   }
+
+  strength: number = DnDCharacter.generateAbilityScore()
+  dexterity: number = DnDCharacter.generateAbilityScore()
+  constitution: number = DnDCharacter.generateAbilityScore()
+  intelligence: number = DnDCharacter.generateAbilityScore()
+  wisdom: number = DnDCharacter.generateAbilityScore()
+  charisma: number = DnDCharacter.generateAbilityScore()
+  
+  hitpoints: number = 10 + DnDCharacter.getModifierFor(this.constitution)
 }
