c197f3d08b9ac9c0a7684672da837bcf52a3c918
chowie <christopher.howie@gmail.com>
2023-03-29 06:30:04 +0800

    Passed tests

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,22 @@
-export const colorCode = () => {
-  throw new Error("Delete this line and implement this function");
+export const colorCode = (input: Color): number => {
+    const value = COLORS.indexOf(input);
+    if (value === undefined) {
+        throw new Error(`Invalid color: ${input}`);
+    }
+    return value;
 };
 
-export const COLORS = undefined;
+export type Color = typeof COLORS[number];
+
+export const COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+];
