3bc800f2f0289474765316dbc58c6803cfce08f6
chowie <christopher.howie@gmail.com>
2023-03-29 06:57:33 +0800

    Passed tests

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,17 @@
-export function toRna() {
-  throw new Error("Remove this statement and implement this function");
+export function toRna(input: string): string {
+    let output = "";
+    for (let i = 0; i < input.length; i++) {
+        if (!DNAtoRNA.has(input.charAt(i))) {
+            throw new Error("Invalid input DNA.");
+        }
+        output += DNAtoRNA.get(input[i]);
+    }
+    return output;
 }
+
+export const DNAtoRNA = new Map<string, string>([
+    ["G", "C"],
+    ["C", "G"],
+    ["T", "A"],
+    ["A", "U"],
+]);
