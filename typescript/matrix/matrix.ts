ff22f88da443e4d3d354cf153bcab652e5b6a4c8
chowie <christopher.howie@gmail.com>
2023-03-29 05:52:53 +0800

    Initial completion

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,19 @@
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
-  }
+    constructor(public input: string) {}
 
-  get rows(): unknown {
-    throw new Error("Remove this statement and implement this function");
-  }
+    get rows(): number[][] {
+        return this.input
+            .split("\n")
+            .map((x) => x.split(" "))
+            .map((x) => x.map((x) => Number(x)));
+    }
 
-  get columns(): unknown {
-    throw new Error("Remove this statement and implement this function");
-  }
+    get columns(): number[][] {
+        let matrix = this.rows;
+        let cols: number[][] = [];
+        for (let i = 0; i < matrix[0].length; i++) {
+            cols[i] = matrix.map((x) => x[i]);
+        }
+        return cols;
+    }
 }
