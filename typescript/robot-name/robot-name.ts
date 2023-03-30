ba17cdd06e8f82b784c510eafa4c4c8f7d4777c4
chowie <christopher.howie@gmail.com>
2023-03-31 04:43:18 +0800

    Passed tests

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,15 +1,41 @@
+function randomUpper(): string {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
 export class Robot {
-  constructor() {}
+    private static names: Set<string> = new Set();
+    private _name: string;
 
-  public get name(): string {
-    throw new Error("Implement Robot#name");
-  }
+    constructor() {
+        this._name = this.generateUniqueName();
+        Robot.names.add(this._name);
+    }
 
-  public resetName(): void {
-    throw new Error("Implement Robot#resetName");
-  }
+    private generateUniqueName(): string {
+        let name: string;
+        do {
+            name =
+                randomUpper() +
+                randomUpper() +
+                Math.floor(Math.random() * 10) +
+                Math.floor(Math.random() * 10) +
+                Math.floor(Math.random() * 10);
+        } while (Robot.names.has(name));
+        return name;
+    }
 
-  public static releaseNames(): void {
-    throw new Error("Implement Robot.releaseNames");
-  }
+    public get name(): string {
+        return this._name;
+    }
+
+    public resetName(): void {
+        const newName = this.generateUniqueName();
+        Robot.names.add(newName);
+        // Robot.names.delete(this._name);
+        this._name = newName;
+    }
+
+    public static releaseNames(): void {
+        Robot.names = new Set();
+    }
 }
