db284cf6eb9b640d8ef63318cb5c25e709c1a7f2
chowie <christopher.howie@gmail.com>
2023-04-03 19:10:17 +0100

    Added reuse of grade(), and sorting on the fly rather than on add

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,30 +1,27 @@
 export class GradeSchool {
-    private _roster: Record<number, string[]> = {};
+    private _roster: Record<number, Set<string>> = {};
 
     roster(): Record<number, string[]> {
         const rosterCopy: Record<number, string[]> = {};
         for (const grade in this._roster) {
-            rosterCopy[grade] = [...this._roster[grade]];
+            rosterCopy[grade] = this.grade(Number(grade));
         }
         return rosterCopy;
     }
 
     add(name: string, grade: number) {
         for (const grade in this._roster) {
-            this._roster[grade] = this._roster[grade].filter(
-                (student) => student !== name
-            );
+            this._roster[grade].delete(name);
         }
 
         if (this._roster[grade]) {
-            this._roster[grade].push(name);
-            this._roster[grade].sort();
+            this._roster[grade].add(name);
         } else {
-            this._roster[grade] = [name];
+            this._roster[grade] = new Set([name]);
         }
     }
 
     grade(grade: number) {
-        return [...(this._roster[grade] ?? [])];
+        return Array.from(this._roster[grade] ?? []).sort();
     }
 }
