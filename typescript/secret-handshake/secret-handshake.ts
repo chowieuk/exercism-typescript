415a8cccab6254548ecc750cadcd455a8c595608
chowie <christopher.howie@gmail.com>
2023-04-04 10:18:06 +0100

    3rd iteration - using bitwise and

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,21 +1,22 @@
-export function commands(decimal_input: number): string[] {
+export function commands(decimal: number): string[] {
     const commands = [
-        { action: "wink" },
-        { action: "double blink" },
-        { action: "close your eyes" },
-        { action: "jump" },
+        { value: 1, action: "wink" },
+        { value: 2, action: "double blink" },
+        { value: 4, action: "close your eyes" },
+        { value: 8, action: "jump" },
     ];
-    let binarySequence = decimal_input.toString(2);
+    const reverseOrder = 16;
+
     let result: string[] = [];
 
-    for (let i = binarySequence.length - 1, j = 0; i >= 0; i--, j++) {
-        if (binarySequence[i] === "1") {
-            if (j < commands.length) {
-                result.push(commands[j].action);
-            } else {
-                result.reverse();
-            }
+    for (const command of commands) {
+        if (decimal & command.value) {
+            result.push(command.action);
         }
+    }
+
+    if (decimal & reverseOrder) {
+        result.reverse();
     }
 
     return result;
