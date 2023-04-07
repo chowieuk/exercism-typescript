4627b323787ffcb57eec8c94245ecb2eddd0e5e1
chowie <christopher.howie@gmail.com>
2023-04-07 08:39:01 +0100

    Initial commit

---
+++ typescript/word-count/word-count.ts
@@ -0,0 +1,141 @@
+export function count(words: string): Map<string, number> {
+
+        
+
+          
+
+  let ret = new Map()
+
+        
+
+          
+
+
+        
+
+          
+
+  words
+
+        
+
+          
+
+    .replace(/[\n\t\r!"#$%&()*+,\-./:;<=>@[\]^_`{|}~]/gm, ' ') // remove all characters that are haram according to test cases
+
+        
+
+          
+
+    .replace(/\s+(?: )/g, ' ') // remove extra spacex, leaving only single space between words
+
+        
+
+          
+
+    .toLowerCase()
+
+        
+
+          
+
+    .trim()
+
+        
+
+          
+
+    .split(' ')
+
+        
+
+          
+
+    .forEach(e => {
+
+        
+
+          
+
+      e = e
+
+        
+
+          
+
+        .replace(/^'/, '') // remove apostrophy at the beginning of the word
+
+        
+
+          
+
+        .replace(/'$/, '') // -------||-------- at the end
+
+        
+
+          
+
+
+        
+
+          
+
+      if(e.length === 0) return
+
+        
+
+          
+
+
+        
+
+          
+
+      if(ret.has(e)) {
+
+        
+
+          
+
+        ret.set(e, ret.get(e) + 1)
+
+        
+
+          
+
+      } else {
+
+        
+
+          
+
+        ret.set(e, 1)
+
+        
+
+          
+
+      }
+
+        
+
+          
+
+    })
+
+        
+
+          
+
+
+        
+
+          
+
+  return ret
+
+        
+
+          
+
+}
