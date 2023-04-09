2e9e639070774a6ba89bd35fb4dc66e4d5a02a41
chowie <christopher.howie@gmail.com>
2023-04-09 17:03:08 +0100

    First iteration

--- typescript/atbash-cipher/atbash-cipher.test.ts
+++ typescript/atbash-cipher/atbash-cipher.test.ts
@@ -9,37 +9,37 @@ describe("AtbashCipher", () => {
             expect(cipherText).toEqual("bvh");
         });
 
-        it.ignore("encode no", () => {
+        it("encode no", () => {
             const cipherText = encode("no");
             expect(cipherText).toEqual("ml");
         });
 
-        it.ignore("encode OMG", () => {
+        it("encode OMG", () => {
             const cipherText = encode("OMG");
             expect(cipherText).toEqual("lnt");
         });
 
-        it.ignore("encode spaces", () => {
+        it("encode spaces", () => {
             const cipherText = encode("O M G");
             expect(cipherText).toEqual("lnt");
         });
 
-        it.ignore("encode mindblowingly", () => {
+        it("encode mindblowingly", () => {
             const cipherText = encode("mindblowingly");
             expect(cipherText).toEqual("nrmwy oldrm tob");
         });
 
-        it.ignore("encode numbers", () => {
+        it("encode numbers", () => {
             const cipherText = encode("Testing,1 2 3, testing.");
             expect(cipherText).toEqual("gvhgr mt123 gvhgr mt");
         });
 
-        it.ignore("encode deep thought", () => {
+        it("encode deep thought", () => {
             const cipherText = encode("Truth is fiction.");
             expect(cipherText).toEqual("gifgs rhurx grlm");
         });
 
-        it.ignore("encode all the letters", () => {
+        it("encode all the letters", () => {
             const cipherText = encode("thequickbrownfoxjumpsoverthelazydog");
             expect(cipherText).toEqual(
                 "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
@@ -48,22 +48,22 @@ describe("AtbashCipher", () => {
     });
 
     describe("decode", () => {
-        it.ignore("decode exercism", () => {
+        it("decode exercism", () => {
             const plainText = decode("vcvix rhn");
             expect(plainText).toEqual("exercism");
         });
 
-        it.ignore("decode a sentence", () => {
+        it("decode a sentence", () => {
             const cipherText = decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v");
             expect(cipherText).toEqual("anobstacleisoftenasteppingstone");
         });
 
-        it.ignore("decode numbers", () => {
+        it("decode numbers", () => {
             const plainText = decode("gvhgr mt123 gvhgr mt");
             expect(plainText).toEqual("testing123testing");
         });
 
-        it.ignore("decode all the letters", () => {
+        it("decode all the letters", () => {
             const cipherText = decode(
                 "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
             );
--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,7 +1,30 @@
-export function encode(plainText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const transpose = {
+    plain: "abcdefghijklmnopqrstuvwxyz1234567890",
+    cipher: "zyxwvutsrqponmlkjihgfedcba1234567890",
+};
+
+export function encode(plainText: string, groupSize: number = 5): string {
+    const sanitzedText = plainText.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let result = "";
+    let counter = 0;
+    for (const character of sanitzedText) {
+        const index = transpose.plain.indexOf(character);
+        result += transpose.cipher[index];
+
+        counter++;
+        if (counter % groupSize === 0 && counter !== sanitzedText.length) {
+            result += " ";
+        }
+    }
+    return result;
 }
 
-export function decode(cipherText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function decode(cipherText: string): string {
+    const sanitzedText = cipherText.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let result = "";
+    for (const character of sanitzedText) {
+        const index = transpose.cipher.indexOf(character);
+        result += transpose.plain[index];
+    }
+    return result;
 }
