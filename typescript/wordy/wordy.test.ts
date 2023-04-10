ccfe35c00a9c7473d64c0b9a167330421217fd61
chowie <christopher.howie@gmail.com>
2023-04-10 15:22:13 +0100

    Incomplete solution using regex

--- typescript/wordy/wordy.test.ts
+++ typescript/wordy/wordy.test.ts
@@ -1,105 +1,101 @@
-import { answer } from './wordy.ts'
-import { describe, it } from 'https://deno.land/std@0.177.0/testing/bdd.ts'
-import { expect } from 'https://deno.land/x/expect@v0.3.0/mod.ts'
+import { answer } from "./wordy.ts";
+import { describe, it } from "https://deno.land/std@0.177.0/testing/bdd.ts";
+import { expect } from "https://deno.land/x/expect@v0.3.0/mod.ts";
 
-describe('Wordy', () => {
-  it('just a number', () => {
-    expect(answer('What is 5?')).toEqual(5)
-  })
+describe("Wordy", () => {
+    it("just a number", () => {
+        expect(answer("What is 5?")).toEqual(5);
+    });
 
-  it.ignore('addition', () => {
-    expect(answer('What is 1 plus 1?')).toEqual(2)
-  })
+    it("addition", () => {
+        expect(answer("What is 1 plus 1?")).toEqual(2);
+    });
 
-  it.ignore('more addition', () => {
-    expect(answer('What is 53 plus 2?')).toEqual(55)
-  })
+    it("more addition", () => {
+        expect(answer("What is 53 plus 2?")).toEqual(55);
+    });
 
-  it.ignore('addition with negative numbers', () => {
-    expect(answer('What is -1 plus -10?')).toEqual(-11)
-  })
+    it("addition with negative numbers", () => {
+        expect(answer("What is -1 plus -10?")).toEqual(-11);
+    });
 
-  it.ignore('large addition', () => {
-    expect(answer('What is 123 plus 45678?')).toEqual(45801)
-  })
+    it("large addition", () => {
+        expect(answer("What is 123 plus 45678?")).toEqual(45801);
+    });
 
-  it.ignore('subtraction', () => {
-    expect(answer('What is 4 minus -12?')).toEqual(16)
-  })
+    it("subtraction", () => {
+        expect(answer("What is 4 minus -12?")).toEqual(16);
+    });
 
-  it.ignore('multiplication', () => {
-    expect(answer('What is -3 multiplied by 25?')).toEqual(-75)
-  })
+    it("multiplication", () => {
+        expect(answer("What is -3 multiplied by 25?")).toEqual(-75);
+    });
 
-  it.ignore('division', () => {
-    expect(answer('What is 33 divided by -3?')).toEqual(-11)
-  })
+    it("division", () => {
+        expect(answer("What is 33 divided by -3?")).toEqual(-11);
+    });
 
-  it.ignore('multiple additions', () => {
-    expect(answer('What is 1 plus 1 plus 1?')).toEqual(3)
-  })
+    it("multiple additions", () => {
+        expect(answer("What is 1 plus 1 plus 1?")).toEqual(3);
+    });
 
-  it.ignore('addition and subtraction', () => {
-    expect(answer('What is 1 plus 5 minus -2?')).toEqual(8)
-  })
+    it("addition and subtraction", () => {
+        expect(answer("What is 1 plus 5 minus -2?")).toEqual(8);
+    });
 
-  it.ignore('multiple subtraction', () => {
-    expect(answer('What is 20 minus 4 minus 13?')).toEqual(3)
-  })
+    it("multiple subtraction", () => {
+        expect(answer("What is 20 minus 4 minus 13?")).toEqual(3);
+    });
 
-  it.ignore('subtraction then addition', () => {
-    expect(answer('What is 17 minus 6 plus 3?')).toEqual(14)
-  })
+    it("subtraction then addition", () => {
+        expect(answer("What is 17 minus 6 plus 3?")).toEqual(14);
+    });
 
-  it.ignore('multiple multiplication', () => {
-    expect(answer('What is 2 multiplied by -2 multiplied by 3?')).toEqual(-12)
-  })
+    it("multiple multiplication", () => {
+        expect(answer("What is 2 multiplied by -2 multiplied by 3?")).toEqual(
+            -12
+        );
+    });
 
-  it.ignore('addition and multiplication', () => {
-    expect(answer('What is -3 plus 7 multiplied by -2?')).toEqual(-8)
-  })
+    it("addition and multiplication", () => {
+        expect(answer("What is -3 plus 7 multiplied by -2?")).toEqual(-8);
+    });
 
-  it.ignore('multiple division', () => {
-    expect(answer('What is -12 divided by 2 divided by -3?')).toEqual(2)
-  })
+    it("multiple division", () => {
+        expect(answer("What is -12 divided by 2 divided by -3?")).toEqual(2);
+    });
 
-  it.ignore('unknown operation', () => {
-    expect(() => answer('What is 52 cubed?')).toThrow(
-      new Error('Unknown operation')
-    )
-  })
+    it("unknown operation", () => {
+        expect(() => answer("What is 52 cubed?")).toThrow("Unknown operation");
+    });
 
-  it.ignore('Non math question', () => {
-    expect(() => answer('Who is the President of the United States?')).toThrow(
-      new Error('Unknown operation')
-    )
-  })
+    it("Non math question", () => {
+        expect(() =>
+            answer("Who is the President of the United States?")
+        ).toThrow("Unknown operation");
+    });
 
-  it.ignore('reject problem missing an operand', () => {
-    expect(() => answer('What is 1 plus?')).toThrow('Syntax error')
-  })
+    it("reject problem missing an operand", () => {
+        expect(() => answer("What is 1 plus?")).toThrow("Syntax error");
+    });
 
-  it.ignore('reject problem with no operands or operators', () => {
-    expect(() => answer('What is?')).toThrow('Syntax error')
-  })
+    it.ignore("reject problem with no operands or operators", () => {
+        expect(() => answer("What is?")).toThrow("Syntax error");
+    });
 
-  it.ignore('reject two operations in a row', () => {
-    expect(() => answer('What is 1 plus plus 2?')).toThrow(
-      new Error('Syntax error')
-    )
-  })
+    it.ignore("reject two operations in a row", () => {
+        expect(() => answer("What is 1 plus plus 2?")).toThrow("Syntax error");
+    });
 
-  it.ignore('reject two numbers in a row', () => {
-    expect(() => answer('What is 1 plus 2 1?')).toThrow(
-      new Error('Syntax error')
-    )
-  })
+    it.ignore("reject two numbers in a row", () => {
+        expect(() => answer("What is 1 plus 2 1?")).toThrow("Syntax error");
+    });
 
-  it.ignore('reject postfix notation', () => {
-    expect(() => answer('What is 1 2 plus?')).toThrow('Syntax error')
-  })
+    it.ignore("reject postfix notation", () => {
+        expect(() => answer("What is 1 2 plus?")).toThrow("Syntax error");
+    });
 
-  it.ignore('reject prefix notation', () => {
-    expect(() => answer('What is plus 1 2?')).toThrow('Syntax error')
-  })
-})
+    it.ignore("reject prefix notation", () => {
+        expect(() => answer("What is plus 1 2?")).toThrow("Syntax error");
+    });
+});
--- typescript/wordy/wordy.ts
+++ typescript/wordy/wordy.ts
@@ -1,3 +1,73 @@
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function')
+interface ParsedQuestion {
+    numbers: number[];
+    operators: string[];
 }
+
+function parseQuestion(input: string): ParsedQuestion {
+    const pattern =
+        /(?:^What is|(?<=\s))(plus|minus|multiplied by|divided by)?\s+(-?\d+)/g;
+
+    let match;
+    const matches: string[] = [];
+
+    while ((match = pattern.exec(input)) !== null) {
+        if (match[1]) matches.push(match[1]);
+        matches.push(match[2]);
+    }
+
+    if (matches.length === 0) {
+        throw new Error("Unknown operation");
+    }
+
+    const numbers: number[] = [];
+    const operators: string[] = [];
+
+    for (let i = 0; i < matches.length; i++) {
+        if (i === 0 || i % 2 === 1) {
+            if (matches[i]) {
+                operators.push(matches[i]);
+            }
+        } else {
+            if (matches[i]) {
+                numbers.push(parseInt(matches[i], 10));
+            }
+        }
+    }
+
+    // if (operators.length + 1 !== numbers.length) {
+    //     throw new Error("Syntax error");
+    // }
+
+    return { numbers, operators };
+}
+
+export const answer = (question: string): number => {
+    const parsedQuestion = parseQuestion(question);
+    const { numbers, operators } = parsedQuestion;
+
+    let result = numbers[0];
+
+    for (let i = 0; i < operators.length; i++) {
+        const operator = operators[i];
+        const nextNumber = numbers[i + 1];
+
+        switch (operator) {
+            case "plus":
+                result += nextNumber;
+                break;
+            case "minus":
+                result -= nextNumber;
+                break;
+            case "multiplied by":
+                result *= nextNumber;
+                break;
+            case "divided by":
+                result /= nextNumber;
+                break;
+            default:
+                throw new Error("Syntax Error");
+        }
+    }
+
+    return result;
+};
