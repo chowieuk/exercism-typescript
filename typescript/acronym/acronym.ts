6fe51316ec9dd57036859761b46c6c0d67d39829
chowie <christopher.howie@gmail.com>
2023-04-12 23:23:24 +0100

    2nd iteration - using map function

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,10 +1,5 @@
 export function parse(phrase: string): string {
     const tokens = phrase.split(/[\s-]+|(?<=[a-z])(?=[A-Z])/);
-    let result = "";
-    let i = 0;
-    while (i < tokens.length) {
-        result += tokens[i][0].toUpperCase();
-        i++;
-    }
-    return result;
+
+    return tokens.map((token) => token[0].toUpperCase()).join("");
 }
