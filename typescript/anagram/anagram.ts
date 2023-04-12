51b787e09fa5d61a1dc93b921082ea5ecb980c09
chowie <christopher.howie@gmail.com>
2023-04-12 23:50:12 +0100

    Passing tests

--- typescript/anagram/anagram.ts
+++ typescript/anagram/anagram.ts
@@ -1,9 +1,59 @@
 export class Anagram {
-  constructor(input: unknown) {
-    throw new Error('Remove this statement and implement this function')
-  }
+    private word: string;
+    private freqMap: Record<string, number>;
+    constructor(input: string) {
+        this.word = input.toLowerCase();
+        this.freqMap = this.createFrequencyMap(this.word);
+    }
 
-  public matches(...potentials: unknown[]): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
+    private createFrequencyMap(str: string): Record<string, number> {
+        const frequencyMap: Record<string, number> = {};
+
+        for (const char of str) {
+            frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+        }
+
+        return frequencyMap;
+    }
+
+    private compareFrequencyMaps(
+        map1: Record<string, number>,
+        map2: Record<string, number>
+    ): boolean {
+        const keys1 = Object.keys(map1);
+        const keys2 = Object.keys(map2);
+
+        if (keys1.length !== keys2.length) {
+            return false;
+        }
+
+        for (const key of keys1) {
+            if (map1[key] !== map2[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public matches(...potentials: string[]): string[] {
+        let matches = [];
+        let i = 0;
+        while (i < potentials.length) {
+            if (potentials[i].toLowerCase() === this.word) {
+                i++;
+                continue;
+            }
+            if (
+                this.compareFrequencyMaps(
+                    this.freqMap,
+                    this.createFrequencyMap(potentials[i].toLowerCase())
+                )
+            ) {
+                matches.push(potentials[i]);
+            }
+            i++;
+        }
+        return matches;
+    }
 }
