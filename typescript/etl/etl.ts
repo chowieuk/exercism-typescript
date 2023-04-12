b5ca2ed1b298e63b4112615bc28496fd80413bf1
chowie <christopher.howie@gmail.com>
2023-04-12 22:58:11 +0100

    passing tests

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,3 +1,21 @@
-export function transform(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+interface OldFormat {
+    [key: string]: string[];
+}
+
+interface NewFormat {
+    [key: string]: number;
+}
+
+export function transform(old: OldFormat): NewFormat {
+    const result: NewFormat = {};
+
+    for (const pointValue in old) {
+        const letters = old[pointValue];
+
+        for (const letter of letters) {
+            result[letter.toLowerCase()] = parseInt(pointValue, 10);
+        }
+    }
+
+    return result;
 }
