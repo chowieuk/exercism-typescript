644f7f8dec1451f99883e0400ded741ac8a050b9
chowie <christopher.howie@gmail.com>
2023-04-12 22:21:45 +0100

    passing tests

--- typescript/nucleotide-count/nucleotide-count.ts
+++ typescript/nucleotide-count/nucleotide-count.ts
@@ -1,3 +1,20 @@
-export function nucleotideCounts(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+interface NucleotideCount {
+    A: number;
+    C: number;
+    G: number;
+    T: number;
+}
+
+export function nucleotideCounts(dnaSequence: string): NucleotideCount {
+    const count: NucleotideCount = { A: 0, C: 0, G: 0, T: 0 };
+
+    for (const nucleotide of dnaSequence) {
+        if (!count.hasOwnProperty(nucleotide)) {
+            throw new Error("Invalid nucleotide in strand");
+        }
+
+        count[nucleotide as keyof NucleotideCount]++;
+    }
+
+    return count;
 }
