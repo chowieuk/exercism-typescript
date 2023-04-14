4c337358a331a6f0fe58eef44d60a38eb4a69b50
chowie <christopher.howie@gmail.com>
2023-04-14 17:19:44 +0100

    passing tests

--- typescript/perfect-numbers/perfect-numbers.ts
+++ typescript/perfect-numbers/perfect-numbers.ts
@@ -1,3 +1,33 @@
-export function classify() {
-  throw new Error('Remove this statement and implement this function')
+export function classify(input: number): string {
+    if (input <= 0) {
+        throw new Error("Classification is only possible for natural numbers.");
+    }
+
+    if (input === 1) {
+        return "deficient";
+    }
+
+    const factors = getFactors(input);
+    const aliquotSum = factors.reduce((sum, factor) => sum + factor, 0);
+
+    if (aliquotSum === input) {
+        return "perfect";
+    } else if (aliquotSum > input) {
+        return "abundant";
+    } else {
+        return "deficient";
+    }
+}
+
+function getFactors(number: number): number[] {
+    const factors = [];
+    for (let i = 1; i <= Math.floor(Math.sqrt(number)); i++) {
+        if (number % i === 0) {
+            factors.push(i);
+            if (i !== number / i && i !== 1) {
+                factors.push(number / i);
+            }
+        }
+    }
+    return factors;
 }
