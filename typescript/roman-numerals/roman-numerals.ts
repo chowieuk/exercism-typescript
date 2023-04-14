2331e577b5656779d6aaff0d15a4f1451109e4d0
chowie <christopher.howie@gmail.com>
2023-04-14 18:19:34 +0100

    2nd iteration - using array

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,31 +1,28 @@
 export const toRoman = (input: number): string => {
-    const romanNumerals: Record<number, string> = {
-        1000: "M",
-        900: "CM",
-        500: "D",
-        400: "CD",
-        100: "C",
-        90: "XC",
-        50: "L",
-        40: "XL",
-        10: "X",
-        9: "IX",
-        5: "V",
-        4: "IV",
-        1: "I",
-    };
+    const romanNumerals: [number, string][] = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"],
+    ];
 
     const convertToRoman = (remaining: number, result: string): string => {
         if (remaining === 0) {
             return result;
         }
 
-        for (const key of Object.keys(romanNumerals).map(Number)) {
+        for (const [key, value] of romanNumerals) {
             if (remaining >= key) {
-                return convertToRoman(
-                    remaining - key,
-                    result + romanNumerals[key]
-                );
+                return convertToRoman(remaining - key, result + value);
             }
         }
 
