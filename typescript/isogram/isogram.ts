3075a1007cdf70a165cb98687bd70955314addbd
chowie <christopher.howie@gmail.com>
2023-04-14 17:29:12 +0100

    2nd iteration - using Set

--- typescript/isogram/isogram.ts
+++ typescript/isogram/isogram.ts
@@ -1,25 +1,17 @@
-function createIsogramFrequencyMap(str: string): Record<string, number> {
-    const frequencyMap: Record<string, number> = {};
-
-    for (const char of str) {
-        if (char !== "-" && char !== " ") {
-            const lowerChar = char.toLowerCase();
-            frequencyMap[lowerChar] = (frequencyMap[lowerChar] ?? 0) + 1;
-        }
-    }
-
-    return frequencyMap;
-}
-
-function hasRepeatingValues(freqMap: Record<string, number>): boolean {
-    for (const key in freqMap) {
-        if (freqMap[key] > 1) {
-            return true;
-        }
-    }
-    return false;
-}
-
 export function isIsogram(input: string): boolean {
-    return !hasRepeatingValues(createIsogramFrequencyMap(input));
+    const seenChars = new Set<string>();
+
+    for (const char of input) {
+        if (char === "-" || char === " ") {
+            continue;
+        }
+
+        const lowerChar = char.toLowerCase();
+        if (seenChars.has(lowerChar)) {
+            return false;
+        }
+        seenChars.add(lowerChar);
+    }
+
+    return true;
 }
